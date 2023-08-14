282dae173f396ff6e05fb30120429c62b5b57d12
EMRD95 <no-reply@dummy.com>
2023-08-15 00:31:37 +0200

    keywords

--- main.js
+++ main.js
@@ -193,48 +193,64 @@ function loadConfig() {
 }
 
 loadConfig();
-// Load initial keywords from config.json and populate input fields
-function loadInitialKeywords() {
-    const allKeywordsInput = document.getElementById("allKeywords");
-    const someKeywordsInput = document.getElementById("someKeywords");
-    const noKeywordsInput = document.getElementById("noKeywords");
-    
-    if (localStorage.getItem("allKeywords")) {
-        allKeywordsInput.value = localStorage.getItem("allKeywords");
-    } else {
-        allKeywordsInput.value = config.allKeywords.join(", ");
-    }
-    
-    if (localStorage.getItem("someKeywords")) {
-        someKeywordsInput.value = localStorage.getItem("someKeywords");
-    } else {
-        someKeywordsInput.value = config.someKeywords.join(", ");
-    }
-    
-    if (localStorage.getItem("noKeywords")) {
-        noKeywordsInput.value = localStorage.getItem("noKeywords");
-    } else {
-        noKeywordsInput.value = config.noKeywords.join(", ");
+
+// Load keywords from config.json and populate input fields
+async function loadKeywordsFromConfig() {
+    try {
+        const response = await fetch(jsonConfigUrl);
+        const configData = await response.json();
+
+        // Populate input fields
+        document.getElementById('allKeywords').value = configData.allKeywords.join(', ');
+        document.getElementById('someKeywords').value = configData.someKeywords.join(', ');
+        document.getElementById('noKeywords').value = configData.noKeywords.join(', ');
+
+        // Load any saved changes from local storage
+        loadChangesFromLocalStorage();
+    } catch (error) {
+        console.error('Failed to load keywords from config.json:', error);
     }
 }
 
-// Save changes to local storage and reflect changes immediately
-function handleKeywordChange() {
-    const allKeywordsInput = document.getElementById("allKeywords");
-    const someKeywordsInput = document.getElementById("someKeywords");
-    const noKeywordsInput = document.getElementById("noKeywords");
-    
-    localStorage.setItem("allKeywords", allKeywordsInput.value);
-    localStorage.setItem("someKeywords", someKeywordsInput.value);
-    localStorage.setItem("noKeywords", noKeywordsInput.value);
-    
-    // TODO: Refresh or filter the articles based on the new keywords
+// Load saved changes from local storage and populate input fields
+function loadChangesFromLocalStorage() {
+    const allKeywords = localStorage.getItem('allKeywords');
+    const someKeywords = localStorage.getItem('someKeywords');
+    const noKeywords = localStorage.getItem('noKeywords');
+
+    if (allKeywords) {
+        document.getElementById('allKeywords').value = allKeywords;
+    }
+    if (someKeywords) {
+        document.getElementById('someKeywords').value = someKeywords;
+    }
+    if (noKeywords) {
+        document.getElementById('noKeywords').value = noKeywords;
+    }
 }
 
-// Add event listeners to the input fields
-document.getElementById("allKeywords").addEventListener("input", handleKeywordChange);
-document.getElementById("someKeywords").addEventListener("input", handleKeywordChange);
-document.getElementById("noKeywords").addEventListener("input", handleKeywordChange);
+// Event listener for the "Apply" button
+document.getElementById('applyChanges').addEventListener('click', () => {
+    // Save changes to local storage
+    localStorage.setItem('allKeywords', document.getElementById('allKeywords').value);
+    localStorage.setItem('someKeywords', document.getElementById('someKeywords').value);
+    localStorage.setItem('noKeywords', document.getElementById('noKeywords').value);
 
-// Call loadInitialKeywords after the config is loaded
-loadInitialKeywords();
+    // Apply filtering logic to articles based on input fields (this functionality needs further integration)
+    // TODO: Implement article filtering logic
+});
+
+// Event listener for the "Reset" button
+document.getElementById('resetChanges').addEventListener('click', () => {
+    // Clear local storage
+    localStorage.removeItem('allKeywords');
+    localStorage.removeItem('someKeywords');
+    localStorage.removeItem('noKeywords');
+
+    // Revert to the default state by reloading keywords from config.json
+    loadKeywordsFromConfig();
+});
+
+// Invoke the function to load keywords from config.json upon page load
+loadKeywordsFromConfig();
+
