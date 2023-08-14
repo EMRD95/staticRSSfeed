7eab12dd7a4ace21f24072926927c00b8794a6bc
EMRD95 <no-reply@dummy.com>
2023-08-15 01:18:59 +0200

    keywords

--- main.js
+++ main.js
@@ -66,15 +66,12 @@ function truncateDescription(description) {
 }
 
 
-
-
 function fetchArticles(feedUrls, allKeywords, someKeywords, noKeywords) {
   const progressBar = document.getElementById('progress-bar');
   progressBar.style.width = '0%';
   allKeywords = allKeywords.map(keyword => keyword.toLowerCase());
   someKeywords = someKeywords.map(keyword => keyword.toLowerCase());
   noKeywords = noKeywords.map(keyword => keyword.toLowerCase());
-
   const promises = feedUrls.map((url, index) => {
     const encodedUrl = encodeURIComponent(url);
     const feedApiUrl = `https://api.rss2json.com/v1/api.json?rss_url=${encodedUrl}&api_key=${apiKey}`;
@@ -88,7 +85,6 @@ function fetchArticles(feedUrls, allKeywords, someKeywords, noKeywords) {
       return response.json();
     });
   });
-
 Promise.allSettled(promises)
     .then(results => {
       let articles = results.flatMap(result =>
@@ -100,26 +96,20 @@ Promise.allSettled(promises)
         article.faviconUrl = `https://www.google.com/s2/favicons?domain=${article.link}`; // update the faviconUrl
         return article;
       });
-
       const filteredArticles = articles.filter(article => {
         const title = sanitizeHTML(article.title).replace(/<.*?>/g, '').toLowerCase();
         const description = sanitizeHTML(article.description).replace(/<.*?>/g, '').toLowerCase();
-
         const allKeywordsIncluded = allKeywords.every(keyword => title.includes(keyword) || description.includes(keyword));
         const someKeywordsIncluded = someKeywords.length === 0 || someKeywords.some(keyword => title.includes(keyword) || description.includes(keyword));
         const noKeywordsIncluded = noKeywords.some(keyword => title.includes(keyword) || description.includes(keyword));
-
         return allKeywordsIncluded && someKeywordsIncluded && !noKeywordsIncluded;
       });
-
       const progressBar = document.getElementById('progress-bar');
       progressBar.style.width = '100%'; // Filtering phase is from 50 to 100%
       displayArticles(filteredArticles);
     })
     .catch(error => console.error(error));
 }
-
-
   const progressBar = document.getElementById('progress-bar');
   progressBar.style.width = '0%';
   allKeywords = allKeywords.map(keyword => keyword.toLowerCase());
